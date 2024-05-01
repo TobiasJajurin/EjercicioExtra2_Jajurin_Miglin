a073245d2b7cf2f16b099ddf5ef7bee8084577ea
unknown <48675805@est.ort.edu.ar>
2024-05-01 17:22:04 -0300

    Creacion de clase superHeroe

---
+++ program.cs
@@ -0,0 +1,8 @@
+﻿namespace EjercicioExtra2_Jajurin_Miglin;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ superHeroes.cs
@@ -0,0 +1,25 @@
+class superHeroe {
+    public string NOMBRE{ get; set; }
+    public string CIUDAD{ get; set; }   
+    public double PESO  { get; set; }
+    public double FUERZA { get; set; }
+    public double VELOCIDAD { get; set; }
+
+    public superHeroe (string nombre = "", string ciudad= "", double peso = 0, double fuerza = 0, double velocidad=0)
+    {
+        NOMBRE = nombre;
+        CIUDAD = ciudad;
+        PESO = peso;
+        FUERZA = fuerza;
+        VELOCIDAD = velocidad;
+
+    }
+    public double calcularSkill ()
+    {
+        Random num = new Random();
+        int numero = num.Next (0,10); 
+        double skill;
+        skill = (VELOCIDAD * 0.6)+(FUERZA * 0.8) + numero ;
+        return skill;
+    }
+}
