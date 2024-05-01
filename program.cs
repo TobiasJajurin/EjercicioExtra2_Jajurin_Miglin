41b5ba39a0f34dfbf279aaeb04d6e1b47c940ede
unknown <48675805@est.ort.edu.ar>
2024-05-01 17:29:37 -0300

    Creacion de menu

--- program.cs
+++ program.cs
@@ -3,6 +3,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string nombre=, ciudad=;
+        double peso =, velocidad=, fuerza=;
+        double diferencia;
+        int opccion;
+
+        opccion = ingresarOpccion ("Ingrese que opccion desea");
+    }
+     public static int ingresarOpccion (string m)
+    {
+        int op;
+        do
+        {
+            Console.WriteLine(m);
+            op = int.Parse(Console.ReadLine());
+        } while(op!= 1 && op!= 2 && op!= 3 && op!= 4);
+        return op;
     }
 }
