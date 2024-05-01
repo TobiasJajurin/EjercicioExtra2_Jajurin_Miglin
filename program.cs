a6801bc8b72541e8169e171f1035485853dcea11
unknown <48675805@est.ort.edu.ar>
2024-05-01 19:09:08 -0300

    Agregamos el extra 1 y modularizamos la lucha

--- program.cs
+++ program.cs
@@ -3,8 +3,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        double diferencia, skill1,skill2;
-        int opccion;
+        int opccion, cambio;
+        double velocidadCambiada, fuerzaCambiada;
         superHeroe superHeroe1 = null , superHeroe2 = null;
 
         opccion = ingresarOpccion ("Ingrese que opccion desea");
@@ -23,44 +23,44 @@ class Program
                 break;
 
                 case 3:
-            if(superHeroe1 != null && superHeroe2 != null)
-            {
-                skill1 = superHeroe1.calcularSkill();
-                skill2 = superHeroe2.calcularSkill();
-                Console.WriteLine(skill1);
-                Console.WriteLine(skill2);
-                if (skill1 > skill2)
-                {
-                    
-                    diferencia = skill1 - skill2;
-                    if (diferencia >= 30)
-                    Console.WriteLine ("Gano el " + superHeroe1.NOMBRE + " Por amplia diferencia");
-                    else if (diferencia >= 10)
-                    Console.WriteLine ("Gano el " + superHeroe1.NOMBRE + " Fue muy parejo");
-                    else
-                    Console.WriteLine ("Gano el " + superHeroe1.NOMBRE + " No le sobro nada");
-
-                }
-                else if (skill1 < skill2)
-                {
-                    diferencia = skill2 - skill1;
-                    if (diferencia >= 30)
-                    Console.WriteLine ("Gano el " + superHeroe2.NOMBRE + " Por amplia diferencia");
-                    else if (diferencia >= 10)
-                    Console.WriteLine ("Gano el " + superHeroe2.NOMBRE + " Fue muy parejo");
-                    else
-                    Console.WriteLine ("Gano el " + superHeroe2.NOMBRE + " No le sobro nada");
-                }
-                else
-                {
-                    Console.WriteLine ("EMPATARON BINKER");
-                }
-            }
+                calcularLucha(superHeroe1, superHeroe2);
                 opccion = ingresarOpccion ("Ingrese que opccion desea");
                 break;
 
+                case 5:
+                cambio = IngresarCambio ("Ingrese que superHeroe quiere cambiar");
+                switch(cambio)
+                {
+                    case 1:
+                    velocidadCambiada = IngresarVelocidad("Ingrese el cambio de velocidad");
+                    fuerzaCambiada = IngresarFuerza("Ingrese cambio de fuerza");
+                    superHeroe1.VELOCIDAD = velocidadCambiada;
+                    superHeroe1.FUERZA = fuerzaCambiada;
+                    break;
+
+                    case 2:
+                    velocidadCambiada = IngresarVelocidad("Ingrese el cambio de velocidad");
+                    fuerzaCambiada = IngresarFuerza("Ingrese cambio de fuerza");
+                    superHeroe2.VELOCIDAD = velocidadCambiada;
+                    superHeroe2.FUERZA = fuerzaCambiada;
+                    break;
+                }
+                calcularLucha(superHeroe1, superHeroe2);
+                break;   
+
             }
         }
+    }
+    public static int IngresarCambio(string s)
+    {
+        int op;
+        do
+        {
+            Console.WriteLine(s);
+            op = int.Parse(Console.ReadLine());
+
+        }while (op != 1 && op != 2);
+        return op;
     }
      public static int ingresarOpccion (string m)
     {
@@ -69,7 +69,7 @@ class Program
         {
             Console.WriteLine(m);
             op = int.Parse(Console.ReadLine());
-        } while(op!= 1 && op!= 2 && op!= 3 && op!= 4);
+        } while(op!= 1 && op!= 2 && op!= 3 && op!= 4 && op!= 5);
         return op;
     }
     
@@ -111,6 +111,43 @@ class Program
         double velocidad = IngresarVelocidad ("Ingrese la velocidad del super heroe ");
         double fuerza = IngresarFuerza ("Ingrese la fuerza super heroe ");
         return new superHeroe(nombre, ciudad, peso, velocidad, fuerza);
+    }
+    public static void calcularLucha (superHeroe superHeroe1, superHeroe superHeroe2)
+    {
+        double skill1, skill2, diferencia;
+        if(superHeroe1 != null && superHeroe2 != null)
+            {
+                skill1 = superHeroe1.calcularSkill();
+                skill2 = superHeroe2.calcularSkill();
+                Console.WriteLine(skill1);
+                Console.WriteLine(skill2);
+                if (skill1 > skill2)
+                {
+                    
+                    diferencia = skill1 - skill2;
+                    if (diferencia >= 30)
+                    Console.WriteLine ("Gano el " + superHeroe1.NOMBRE + " Por amplia diferencia");
+                    else if (diferencia >= 10)
+                    Console.WriteLine ("Gano el " + superHeroe1.NOMBRE + " Fue muy parejo");
+                    else
+                    Console.WriteLine ("Gano el " + superHeroe1.NOMBRE + " No le sobro nada");
+
+                }
+                else if (skill1 < skill2)
+                {
+                    diferencia = skill2 - skill1;
+                    if (diferencia >= 30)
+                    Console.WriteLine ("Gano el " + superHeroe2.NOMBRE + " Por amplia diferencia");
+                    else if (diferencia >= 10)
+                    Console.WriteLine ("Gano el " + superHeroe2.NOMBRE + " Fue muy parejo");
+                    else
+                    Console.WriteLine ("Gano el " + superHeroe2.NOMBRE + " No le sobro nada");
+                }
+                else
+                {
+                    Console.WriteLine ("EMPATARON BINKER");
+                }
+            }
     }
 
 }
