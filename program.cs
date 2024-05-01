8f3e9a322beefcb576a50f9b2f1ee2e99fb73409
unknown <48675805@est.ort.edu.ar>
2024-05-01 17:34:35 -0300

    Ingreso de datos en el main

--- program.cs
+++ program.cs
@@ -3,8 +3,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        string nombre=, ciudad=;
-        double peso =, velocidad=, fuerza=;
+        string nombre, ciudad;
+        double peso, velocidad, fuerza;
         double diferencia;
         int opccion;
 
@@ -12,17 +12,21 @@ class Program
         switch (opccion) 
         {
             case 1:
+            IngresoTotalDatos(nombre, ciudad, peso, velocidad, fuerza);
+            opccion = ingresarOpccion ("Ingrese que opccion desea");
             break;
 
             case 2:
+            IngresoTotalDatos(nombre, ciudad, peso, velocidad, fuerza);
+            opccion = ingresarOpccion ("Ingrese que opccion desea");
             break;
 
             case 3:
+            opccion = ingresarOpccion ("Ingrese que opccion desea");
             break;
 
             case 4:
             break;
-
 
 
         }
@@ -37,4 +41,42 @@ class Program
         } while(op!= 1 && op!= 2 && op!= 3 && op!= 4);
         return op;
     }
+    public static void IngresoTotalDatos (string nombre, string ciudad, double peso, double velocidad, double fuerza)
+    {
+         nombre = IngresarString("Ingrese el nombre del super heroe ");
+         ciudad = IngresarString("Ingrese la ciudad del super heroe ");
+         peso = ingresarDouble("Ingrese el peso del super heroe ");
+         velocidad = IngresarVelocidad ("Ingrese la velocidad del super heroe ");
+         fuerza = IngresarFuerza ("Ingrese la fuerza super heroe ");
+         
+    }
+     public static string IngresarString (string m)
+    {
+        return Console.ReadLine();
+    }
+    public static double ingresarDouble (string m)
+    {
+        return double.Parse(Console.ReadLine());    
+    }
+    public static double IngresarVelocidad (string m)
+    {
+        double velocidad;
+        do  
+        {
+            Console.WriteLine(m);
+            velocidad = double.Parse(Console.ReadLine());
+        } while(velocidad < 1 && velocidad > 100);
+        return velocidad;
+    }
+    public static double IngresarFuerza (string m)
+    {
+        double fuerza;
+        do  
+        {
+            Console.WriteLine(m);
+            fuerza = double.Parse(Console.ReadLine());
+        } while(fuerza < 1 && fuerza > 100);
+        return fuerza;
+    }
+
 }
